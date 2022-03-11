5a8ca581615d8f000e26f896476b9612384a73a6
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-03-11 17:07:07 -0300

    11-03/p1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
 
-
+import { createContext, useState } from 'react'
+import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/Home";
+import { BrowserRouter, Route } from 'react-router-dom';
+import { auth, firebase } from './services/firebase'
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWhithGoogle: () => void;
+}
+export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
+  const [user, setUser] = useState<User>();
+
+  function signInWhithGoogle(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result=> {
+      if (result.user){
+        const { displayName, photoURL,uid} = result.user;
+
+        if (!displayName || !photoURL){
+          throw new Error('Missing information from Google Account.');
+        }
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+
+    });
+  }
+
   return (
-    
-      <Home />
+    <BrowserRouter>
+      <AuthContext.Provider value={{ user, signInWhithGoogle }}>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+
+      </AuthContext.Provider>
+    </BrowserRouter>
    
   );
 }
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,23 +1,12 @@
-import { useHistory } from 'react-router-dom'
-import { auth, firebase } from '../services/firebase' 
-
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { Link } from 'react-router-dom';
 
 
-export function Home() {
-
-  const history = useHistory();
-
-  function handleCreateRoom(){
-   
-      history.push('/rooms/new')
-
-  }
-
+export function NewRoom() {
   return (
     <div id="page-auth">
       <aside>
@@ -28,17 +17,14 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="letmeask" />
-          <button className="create-room" onClick={handleCreateRoom}>
-            <img src={googleIconImg} alt="Logo do Google" />
-            Cria sua sala com o Google
-          </button>
-
-          <div className="separator">Ou entre em uma sala</div>
-
+          <h2>Criar uma nova sala</h2>
           <form>
-            <input type="text" placeholder="Digite o código da sala" />
-            <Button type="submit">Entrar na sala</Button>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">Criar sala</Button>
           </form>
+          <p>
+              Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
+          </p>
         </div>
       </main>
     </div>
