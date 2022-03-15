6f8083bbdfa8c61687165be6655ce11607715cb0
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-03-15 13:30:14 -0300

    commit

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,34 @@
+import logoImage from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import '../styles/room.scss'
+
 export function Room() {
     return (
-        <h1>Sala</h1>
+        <div id='page-romm'>
+            <header>
+                <div className='content'>
+                    <img src={logoImage} alt='letmeask' />
+                    <div>codigo</div>
+                </div>
+            </header>
+            <main>
+                <div className='room-title'>
+                    <h1>Sala</h1>
+                    <span>num perguntas</span>
+                </div>
+
+                <form>
+                    <textarea
+                    placeholder="O que deseja perguntar?" />
+
+                    <div className='form-footer'>
+                        <span>Para enviar uma pergunta, <button>faça seu login</button></span>
+                        <Button type="submit">Enviar pergunta</Button>
+                    </div>
+
+                </form>
+
+            </main>
+        </div>
     )
 }
